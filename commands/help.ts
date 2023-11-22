fdd663480f80131428e72986e9584937a5274f44
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-11-22 18:18:58 +0100

    feat: :sparkles: Add category sort in help command

--- commands/help.ts
+++ commands/help.ts
@@ -7,42 +7,97 @@ const help = {
   permissions: null,
   category: "Info",
   dm: true,
-  options: [],
+  options: [
+    {
+      name: "category",
+      description: "The category of the command.",
+      type: "string",
+      required: false,
+    },
+  ],
   async run(client: HeavensBot, interaction: CommandInteraction) {
-    const categories: string[] = [];
-    client.commands.forEach((command) => {
-      if (!categories.includes(command.category?.toString()! as never)) {
-        categories.push(command.category?.toString()! as never);
-      }
-    });
-    await interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("Help")
-          .setDescription("Here is a list of commands.")
-          .addFields(
-            categories.map((category) => {
-              return {
-                name: category,
-                value: client.commands
-                  .filter((command) => command.category === category)
-                  .map((command) => `\`${command.name}\``)
-                  .join(", "),
-              };
-            })
+    if (
+      interaction.options.get("category") === null ||
+      interaction.options.get("category") === undefined
+    ) {
+      const categories: string[] = [];
+      client.commands.forEach((command) => {
+        if (!categories.includes(command.category?.toString()! as never)) {
+          categories.push(command.category?.toString()! as never);
+        }
+      });
+      await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("Help")
+            .setDescription("Here is a list of commands.")
+            .addFields(
+              categories.map((category) => {
+                return {
+                  name: category,
+                  value: client.commands
+                    .filter((command) => command.category === category)
+                    .map((command) => `\`${command.name}\``)
+                    .join(", "),
+                };
+              })
+            )
+            .setColor("Random")
+            .setTimestamp()
+            .setFooter({
+              text: `©️ ${
+                new Date().getFullYear() === 2023
+                  ? 2023
+                  : `2023-${new Date().getFullYear()}`
+              } ${client.user!.tag}`,
+              iconURL: client.user!.displayAvatarURL(),
+            }),
+        ],
+      });
+    } else {
+      const category: string = interaction.options
+        .get("category")!
+        .value!.toString();
+
+      if (
+        category.toLowerCase() === "info" ||
+        category.toLowerCase() === "moderation"
+      ) {
+        const commands = client.commands
+          .filter(
+            (command) =>
+              command.category ===
+              category.charAt(0).toUpperCase() + category.slice(1)
           )
-          .setColor("Random")
-          .setTimestamp()
-          .setFooter({
-            text: `©️ ${
-              new Date().getFullYear() === 2023
-                ? 2023
-                : `2023-${new Date().getFullYear()}`
-            } ${client.user!.tag}`,
-            iconURL: client.user!.displayAvatarURL(),
-          }),
-      ],
-    });
+          .map((command) => `\`${command.name}\``)
+          .join(", ");
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle("Help")
+              .setDescription(
+                `Here is a list of commands in ${
+                  category.charAt(0).toUpperCase() + category.slice(1)
+                }.`
+              )
+              .addFields({
+                name: category.charAt(0).toUpperCase() + category.slice(1),
+                value: commands,
+              })
+              .setColor("Random")
+              .setTimestamp()
+              .setFooter({
+                text: `©️ ${
+                  new Date().getFullYear() === 2023
+                    ? 2023
+                    : `2023-${new Date().getFullYear()}`
+                } ${client.user!.tag}`,
+                iconURL: client.user!.displayAvatarURL(),
+              }),
+          ],
+        });
+      }
+    }
   },
 };
 
